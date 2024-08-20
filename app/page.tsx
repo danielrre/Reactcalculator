5eda74b27e4fba59be106525437fe804818f1ba3
Daniel <ramirezdaniel.dt@gmail.com>
2024-08-19 20:29:06 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    
+      <h1 className="font-semibold	text-teal-500 text-9xl"> Hello sucker!</h1>
+    
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween padding-container max-container relative z-30 py-5">
+      <Link href="/">
+        <Image src={"/hilink-logo.svg"} alt="logo" width={74} height={29}/>
+        
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
